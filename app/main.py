ecc3b8327a1130bdff379ada022379615b378855
GOavi101 <1704178@kiit.ac.in>
2024-04-23 22:35:54 +0530

    returning 404 for different path

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     client_socket, client_address =server_socket.accept() # wait for client
     
     while True:
@@ -12,8 +12,13 @@ def main():
         if not data:
             # if data is not received break
             break
-        output=b"HTTP/1.1 200 OK\r\n\r\n"
-        client_socket.sendall(output)
+        data_list=data.split("\r\n")
+        path=data_list[0].split(" ")[1]
+        print(str(path))
+        if str(path)=="/":
+            client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
     client_socket.close()  # close the connection
     server_socket.close()
         
