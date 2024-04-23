2bd605f1d35324a4d7c1a93e9d55d50d6ad5b0d6
GOavi101 <1704178@kiit.ac.in>
2024-04-23 21:47:01 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -4,7 +4,18 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address =server_socket.accept() # wait for client
+    
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = client_socket.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        output="HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.send(output)
+    client_socket.close()  # close the connection
+        
 
 
 if __name__ == "__main__":
