358f425399c6e5688caf5ae61ab9a30b3638d56c
GOavi101 <1704178@kiit.ac.in>
2024-04-24 10:42:30 +0530

    GET file data 12

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def handle_client(client_socket,dir):
                 with open(file_path, "rb") as f:
                     data = f.read()
                 contentLength = str(len(data))
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength}\r\n\r\n".encode("utf-8")+data
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength}\r\n\r\n".encode()+data
                 client_socket.sendall(response)
             else:
                 client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")                   
