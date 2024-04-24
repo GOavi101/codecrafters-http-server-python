36a51f3d9ba15b47cb75920e4b45fa3ed9b2c270
GOavi101 <1704178@kiit.ac.in>
2024-04-24 08:34:46 +0530

    adding concurrency

--- app/main.py
+++ app/main.py
@@ -1,35 +1,35 @@
 import socket
+import threading
 
-
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, client_address =server_socket.accept() # wait for client
-    
+def handle_client(client_socket):
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
         data = client_socket.recv(4096).decode()
         if not data:
-            # if data is not received break
             break
-        data_list=data.split("\r\n")
-        path=data_list[0].split(" ")[1]
-        
-        if str(path)=="/":
+        data_list = data.split("\r\n")
+        path = data_list[0].split(" ")[1]
+
+        if str(path) == "/":
             client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         elif "/echo/" in str(path):
-            msg=path.split("/echo/")[-1]
-            response=f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
+            msg = path.split("/echo/")[-1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
             client_socket.sendall(response.encode())
-        elif str(path)=="/user-agent":
-            userInfo=data_list[2].split(" ")[1]
-            response=f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(userInfo)}\r\n\r\n{userInfo}"
+        elif str(path) == "/user-agent":
+            user_info = data_list[2].split(" ")[-1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_info)}\r\n\r\n{user_info}"
             client_socket.sendall(response.encode())
         else:
             client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    client_socket.close()  # close the connection
-    server_socket.close()
-        
+    client_socket.close()
 
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        client_socket, _ = server_socket.accept()
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
 
 if __name__ == "__main__":
     main()
