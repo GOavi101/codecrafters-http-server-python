9b6408cd5ca8695e3cb1b7d56b454cbf1508367d
GOavi101 <1704178@kiit.ac.in>
2024-04-24 10:34:12 +0530

    GET file data 3

--- app/main.py
+++ app/main.py
@@ -26,9 +26,9 @@ def handle_client(client_socket,dir):
             file_path = dir+"/"+filename
     
             if os.path.exists(file_path):
-                with open(file_path, "r") as f:
+                with open(file_path, "rb") as f:
                     data = f.read()
-                contentLength = str(len(data))
+                contentLength = str(len(data)).encode("ascii")
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {contentLength}\r\n\r\n{data}"
                 client_socket.sendall(response.encode("utf-8"))
             else:
